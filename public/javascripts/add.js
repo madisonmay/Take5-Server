15d2beeeeb4fe552e47de643d62157b7ca6f650a
madisonmay13 <madison.may@students.olin.edu>
2013-04-09 16:39:06 -0400

    Preparing to merge

--- public/javascripts/add.js
+++ public/javascripts/add.js
@@ -1,4 +1,12 @@
 $(document).ready(function(){
+
+    var h = $(document).height() / 2;
+    var h2 = $('#login').height();
+
+    $('#login').css('top', parseInt(h-h2) + 'px');
+    console.log("Moved");
+
+
 	console.log("Ready")
 	$('#add').submit(function () {
 		console.log('ready');
