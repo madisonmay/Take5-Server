4d8bbda53902e48aed0b9dcee43f636a413dc009
madisonmay13 <madison.may@students.olin.edu>
2013-04-09 16:23:55 -0400

    Fixed login screen vertAlign

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    var h = $(document).height();
-    var h2 = $('#login').height() * 2;
+    var h = $(document).height() / 2;
+    var h2 = $('#login').height();
 
     $('#login').css('top', parseInt(h-h2) + 'px');
     console.log("Moved")
